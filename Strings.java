fc28bc2d9746100ea5250dba783ad2f23c885995
Anuj B <88391110+AnujBalu@users.noreply.github.com>
2022-12-09 09:49:49 +0530

    Create Strings.java

---
+++ Strings.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+import java.util.StringTokenizer;
+
+class Strings{
+    public static void main(Strings args[]){
+        
+        StringTokenizer st = new StringTokenizer("Hello guys, I am yours crazy newbie", " ");
+        
+        // count the Tokens in the sentance
+        System.out.println("No.of tokens before using nextToken(): "+ st.countTokens());
+        
+        // Printing the splited words
+        while (st.hasMoreTokens()){
+            System.out.println("Tokens are: "+ st.nextToken());
+        }
+        
+        // count the Tokens in the sentance
+        System.out.println("No.of tokens After using nextToken(): "+st.countTokens());
+        
+        // Getting tokenizer from user to StringTikenizer
+        Scanner sn = new Scanner(System.in);
+        System.out.println("Enter a Sentence: ");
+        String user = sn.nextLine(); // getting String from user
+        
+        StringTokenizer st2 = new StringTokenizer(user," ");
+        while(st2.hasMoreTokens()){
+            System.out.println(st2.nextToken());
+        }
+    }
+}
